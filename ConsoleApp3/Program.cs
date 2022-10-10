4203451180e90bb58fe04c0e8842391a25f2c2ba
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-10-10 14:35:28 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine (countdown(0));
+        }
+
+        public static string countdown(int total)
+        {
+            string count = "";
+            while (total >= 0)
+            {
+                count += total;
+                count += " ";
+                total = total - 1;
+
+            }
+            count = count.Remove(count.Length - 1, 1);
+
+            return count;
+        }
+    }
+}
+// place your function here
